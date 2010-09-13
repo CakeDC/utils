2b62420dc31adf8eac5b182ac61fd4f5bd9e3fcc
Olivier Louvignes <olivier@mg-crea.com>
2010-09-13 19:24:37 +0200

    Added multiple fields support, json engine toggle & saveAll support to Serializable behavior

--- models/behaviors/serializable.php
+++ models/behaviors/serializable.php
@@ -20,19 +20,25 @@
 class SerializableBehavior extends ModelBehavior {
 
 /**
+ * Engine switch
+ *
+ * @var boolean
+ */
+	public $json = true;
+
+/**
  * Settings
- * 
+ *
  * @var mixed
  */
 	public $settings = array();
 
 /**
  * Default settings
- * 
+ *
  * @var array
  */
-	protected $_defaults = array(
-		'field' => 'data');
+	protected $_defaults = array();
 
 /**
  * Setup
@@ -40,18 +46,18 @@ class SerializableBehavior extends ModelBehavior {
  * @param object AppModel
  * @param array $config
  */
-	public function setup(&$model, $config = array()) {
+	public function setup(&$Model, $config = array()) {
 		$settings = array_merge($this->_defaults, $config);
-		$this->settings[$model->alias] = $settings;
+		$this->settings[$Model->alias] = $settings;
 	}
-	
+
 /**
  * After find callback
  *
  * @param mixed $results The results of the find operation
  * @param boolean $primary Whether this model is being queried directly (vs. being queried as an association)
  * @return mixed Result of the find operation
- */	
+ */
 	function afterFind($Model, $results, $primary = false) {
 		$config = $this->settings[$Model->alias];
 		if (!empty($results)) {
@@ -71,40 +77,61 @@ class SerializableBehavior extends ModelBehavior {
 		$Model->data = $Model->serialize($Model->data);
 		return true;
 	}
-	
+
 /**
- * 
+ *
  *
  * @param string $matchId
  * @param array $data
  * @return boolean
- */	
+ */
 	public function serialize($Model, &$data) {
-		$config = $this->settings[$Model->alias];
-		if (isset($data[$Model->alias][$config['field']]) && is_array($data[$Model->alias][$config['field']])) {
-			$data[$Model->alias][$config['field']] = @serialize($data[$Model->alias][$config['field']]);
+		if(empty($data[$Model->alias])) return $data;
+		$fields = $this->settings[$Model->alias];
+		if (!empty($data[$Model->alias][0]) && array_intersect_key($fields, array_keys($data[$Model->alias][0]))) {
+			foreach($data[$Model->alias] as $model) {
+				$model = $Model->serialize(array($Model->alias => $model));
+				$data[$Model->alias][0] = $model[$Model->alias];
+			}
+		} elseif(array_intersect_key($fields, array_keys($data[$Model->alias]))) {
+			foreach ($fields as $field) {
+				if (isset($data[$Model->alias][$field]) && is_array($data[$Model->alias][$field])) {
+					if ($this->json) {
+						$data[$Model->alias][$field] = @json_encode($data[$Model->alias][$field]);
+					} else {
+						$data[$Model->alias][$field] = @serialize($data[$Model->alias][$field]);
+					}
+				}
+			}
 		}
 		return $data;
 	}
 
 /**
- * 
+ *
  *
  * @param string $matchId
  * @param array $data
  * @return boolean
- */	
+ */
 	public function deserialize($Model, &$data) {
-		$config = $this->settings[$Model->alias];
-		if (!empty($data[$Model->alias][$config['field']])) {
-			if (is_string($data[$Model->alias][$config['field']])) {
-				$data[$Model->alias][$config['field']] = @unserialize($data[$Model->alias][$config['field']]);
-				if ($data[$Model->alias][$config['field']] === false) {
-					$data[$Model->alias][$config['field']] = array();
+		if(empty($data[$Model->alias])) return $data;
+		$fields = $this->settings[$Model->alias];
+		foreach($fields as $field) {
+			if (!empty($data[$Model->alias][$field])) {
+				if (is_string($data[$Model->alias][$field])) {
+					if ($this->json) {
+						$data[$Model->alias][$field] = @json_decode($data[$Model->alias][$field], true);
+					} else {
+						$data[$Model->alias][$field] = @unserialize($data[$Model->alias][$field]);
+					}
+					if ($data[$Model->alias][$field] === false) {
+						$data[$Model->alias][$field] = array();
+					}
 				}
+			} elseif(array_key_exists($field, $data[$Model->alias])) {
+				$data[$Model->alias][$field] = array();
 			}
-		} else {
-			$data[$Model->alias][$config['field']] = array();
 		}
 		return $data;
 	}
