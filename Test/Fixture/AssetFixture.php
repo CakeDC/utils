fc7a1f29c11c324e8d8c1e3aac2096825f4b2f5e
Florian Krämer <flrorian.kraemer@cakedc.com>
2013-01-21 22:24:14 +0100

    Coding standard improvements for fixtures

--- Test/Fixture/AssetFixture.php
+++ Test/Fixture/AssetFixture.php
@@ -1,25 +1,38 @@
 <?php
-// For the inheritable behavior
+/**
+ * Short description for class.
+ *
+ * @package	   cake
+ * @subpackage	cake.tests.fixtures
+ */
 class AssetFixture extends CakeTestFixture {
-    var $name = 'Asset';
-    
-    var $fields = array(
-        'id' => array('type' => 'integer', 'key' => 'primary'),
-        'title' => array('type' => 'string', 'null' => false),
-        'description' => array('type' => 'text'),
-        'created' => 'datetime',
-        'updated' => 'datetime'
-    );
-    
-    var $records = array(
-    
-        array('id' => 1, 'title'=> 'soccuer image', 'description'=> 'amazing shot...'),
-        array('id' => 2, 'title'=> 'animal image', 'description'=> 'very disturbing'),
 
-        array('id' => 11, 'title'=> 'home page link', 'description' => 'link back to home page'),
-        array('id' => 12, 'title'=> 'google', 'description' => 'Google is the search engine'),
-        
-    );
-    
+/**
+ * name property
+ *
+ * @var string 'AnotherPost'
+ * @access public
+ */
+	public $name = 'Asset';
+
+/**
+ * fields property
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false),
+		'description' => array('type' => 'text'),
+		'created' => 'datetime',
+		'updated' => 'datetime'
+	);
+
+	public $records = array(
+		array('id' => 1, 'title'=> 'soccuer image', 'description'=> 'amazing shot...'),
+		array('id' => 2, 'title'=> 'animal image', 'description'=> 'very disturbing'),
+		array('id' => 11, 'title'=> 'home page link', 'description' => 'link back to home page'),
+		array('id' => 12, 'title'=> 'google', 'description' => 'Google is the search engine'),
+	);
+
 }
-?>
