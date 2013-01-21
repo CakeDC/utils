dcdaddd551d785a09fd153722cf31d43082971d1
Florian Krämer <flrorian.kraemer@cakedc.com>
2013-01-21 13:31:34 +0100

    Refactoring the exception for the PingbackableBehavior

---
+++ Error/TrackbackException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright 2007-2010, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2007-2010, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class RegisterException extends CakeException {
+	public $messageString;
+	function __construct($message) {
+		$this->messageString = $message;
+	}
+}
